dc46ed6aea9612356dfc533343339f1b218acc0a
MuhammadBeyyy <146830245+MuhammadBeyyy@users.noreply.github.com>
2024-01-19 06:03:39 -0800

    Update App.js

--- Islam Q&A/src/App.js
+++ Islam Q&A/src/App.js
@@ -6,11 +6,11 @@ import Questions from './Question'
 export default function App() {
 
   const [question,setQuestion] = useState(Data)
-  console.log(question)
+  //console.log(question)
   return (
         <main>
           <div className='container'>
-            <h3> Frequently asked Questions - Islam</h3>
+            <h3> Frequently asked Questions (FAQ) - Islam</h3>
           <Questions  quest={question}/>
           </div>
         </main>
