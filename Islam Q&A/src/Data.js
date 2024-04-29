5eb9b24904356431ad4683b493567680f777c489
muhammadbeyy <muhammadshittabey@gmail.com>
2024-04-29 11:50:08 -0700

    Update Data.js

--- Islam Q&A/src/Data.js
+++ Islam Q&A/src/Data.js
@@ -1,22 +1,22 @@
 const questions = [
     {
-      id: 1,
-      title: 'What are the Five Pillars in Islam?',
+      id: 11,
+      title: 'What are the Five Pillars 5 in Islam?',
       info:
         'The Five Pillars of Islam are the five basic acts of worship that are considered mandatory for Muslims. They are the declaration of faith (Shahada), prayer (Salah), giving of alms (Zakat), fasting (Sawm), and pilgrimage to Mecca (Hajj)'
     },
     {
-      id: 2,
+      id: 12,
       title: 'Are Muslims allowed to listen to music?',
       info:'In Islam, the permissibility of listening to music is a topic of debate among scholars. Some scholars consider certain types of music to be permissible, while others believe that all music is prohibited. The key factors that influence this debate include the content and purpose of the music, as well as its potential impact on an individuals behavior and spirituality.'
     },
     {
-      id: 3,
+      id: 13,
       title: 'What is the difference between Sunni and Shia Islam or Muslims?',
       info:'The main difference between Sunni and Shia Islam lies in their beliefs regarding the rightful successor to the Prophet Muhammad Sunni Muslims believe that the first four caliphs, including Abu Bakr, Umar, Uthman, and Ali, were the rightful successors to the Prophet Muhammad. They make up the majority of the Muslim population while the Shia Muslims, on the other hand, believe that Ali, the cousin and son-in-law of the Prophet Muhammad, was the rightful successor. They believe in the concept of Imamah, which asserts that leadership should have passed directly to Ali and his descendants'
     },
     {
-      id: 4,
+      id: 14,
       title: 'What is the concept of Jihad in Islam?',
       info:'The concept of Jihad in Islam is often misunderstood and misrepresented. Jihad, in its literal meaning, translates to "struggle" or "striving." In Islamic teachings, Jihad encompasses various forms of struggle, including the internal struggle against ones own desires and temptations, the struggle to improve society and uphold justice, and, when necessary, the physical defense of oneself and the community.',
     },
